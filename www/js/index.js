839274b6ed93ed1642a745203443645fcf0d01f9
Pat Osorio <pat@smartwave.ph>
2022-01-21 19:38:15 +0800

    Updated width and height. Added onclick functions for spineboy

--- www/js/index.js
+++ www/js/index.js
@@ -1,13 +1,16 @@
 
 document.addEventListener('deviceready', function() {
+
+    var deviceWidth = innerWidth * window.devicePixelRatio;
+    var deviceHeight = innerHeight * window.devicePixelRatio;
     var config = {
         type: Phaser.AUTO,//CANVAS,//WEBGL,
-    
-        width: 428,
-        height: 926,
+        width: deviceWidth,
+        height: deviceHeight,
         scene: {
             preload: preload,
-            create: create
+            create: create,
+            update: update
         },
         plugins: {
             scene: [
@@ -20,37 +23,51 @@ document.addEventListener('deviceready', function() {
     
     function preload() {
         // PHASER IMAGE
-        this.load.image('logo', './img/assets/phaser/phaser.png');
+        this.load.image('logo', 'img/assets/phaser/phaser.png');
 
         // SPINE BOY
-        // this.load.setPath('./img/assets/animations/spineboy/');
-        // this.load.spine('sb', 'demos.json', [ 'atlas1.atlas' ], true);
+        this.load.setPath('./img/assets/animations/spineboy/');
+        this.load.spine('sb', 'demos.json', [ 'atlas1.atlas' ], true);
 
         // OOLAF
-        this.load.setPath('./img/assets/animations/oolaf/');
-        this.load.spine('oolaf', 'fd399fada36154b280d11961efa96e33784a2257.json', [ 'fd399fada36154b280d11961efa96e33784a2257.atlas' ], true);
+        // this.load.setPath('img/assets/animations/oolaf/');
+        // this.load.spine('oolaf', 'fd399fada36154b280d11961efa96e33784a2257.json', [ 'fd399fada36154b280d11961efa96e33784a2257.atlas' ], true);
     }
     
     function create() {
         this.add.image(0, 0, 'logo').setOrigin(0);
 
+        this.input.mouse.disableContextMenu();
         // SPINE BOY
-        // var spineObject = this.add.spine(200, 300, 'sb.spineboy', 'death', true);
+        var spineObject = this.add.spine(deviceWidth/2, deviceHeight/2, 'sb.spineboy', 'idle', true).setScale(0.25);
 
         // OOLAF
-        var spineObject = this.add.spine(200, 600, 'oolaf', 'animation', true).setScale(0.15);
+        // var spineObject = this.add.spine(200, 600, 'oolaf', 'animation', true).setScale(0.15);
 
-        console.log(spineObject.getCurrentAnimation());
+        console.log(spineObject.getAnimationList());
         console.log(spineObject);
 
         // RESIZE
         // boy.setDisplaySize(125, 300);
         // boy.setDisplayOrigin(0, 0);
 
+        // this.input.mouse.capture = true;
+
+        this.input.on('pointerdown', (pointer) => {
+            spineObject.play('shoot', false, true);
+        }, this);
+
+        this.input.on('pointerup', () => {
+            spineObject.play('idle', true, true);
+        }, this);
+
         // responsive sizing
         window.addEventListener('resize', resize);
         resize();
     }    
+
+    function update () {
+    }
 
     function resize() {
         var canvas = game.canvas, width = window.innerWidth, height = window.innerHeight;
